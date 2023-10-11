c28a4b20a9dc06223524231ae47753d50af4e0ff
kiran-bea <kiran.j@beanalytic.com>
2023-10-11 17:03:56 +0530

    starting

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'semantic-ui-css/semantic.min.css'
+import formm from './pages/formm'
+
+export default function App() {
+  return (
+    <div>
+      hh
+      <formm />
+    </div>
+  )
+}
---
+++ src/pages/formm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function formm() {
+  return (
+    <div>
+      hello guru
+    </div>
+  )
+}
